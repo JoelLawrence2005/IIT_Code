e2094b49b98310594ced0418407a77113f3d1c2a
JoelLawrence2005 <122905504+JoelLawrence2005@users.noreply.github.com>
2023-10-24 21:29:05 +0530

    Create WHILE_LOOP_&_FOR_LOOP.py
    Just created for loop examples only.

---
+++ WHILE_LOOP_&_FOR_LOOP.py
@@ -0,0 +1,18 @@
+# FOR LOOP
+
+# Example 01
+print(*range(5)) # Prints all the numbers from 0 to 4, the * in this is called a "Unpacking Operator"
+print(*range(5, 20, 3))  # The numbers inside the brackets are called "Arguments"
+                         # the first argument gives the starting value
+                         # second argument gives the stopping value
+                         # third argument gives the step value, basically by how much the sequence should change
+
+# Example 02
+for i in range(3): # Goes through the loop thrice, to print the string "Hello" thrice
+  print("Hello")
+
+# Example 03
+for i in range(2):    # Goes through loop 1, at the start i = 0
+  for j in range(4):  # Goes through loop 2, i = 0 and j = 0
+    print(i, j)       # Prints (i, j) = 0 0, this repeats until the first loop is finished
+
