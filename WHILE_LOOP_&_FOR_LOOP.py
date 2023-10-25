b7492f3117a8801771876f80b766f7d84ce267e7
JoelLawrence2005 <122905504+JoelLawrence2005@users.noreply.github.com>
2023-10-25 19:44:46 +0530

    Updated WHILE_LOOP_&_FOR_LOOP.py

--- WHILE_LOOP_&_FOR_LOOP.py
+++ WHILE_LOOP_&_FOR_LOOP.py
@@ -16,3 +16,21 @@ for i in range(2):    # Goes through loop 1, at the start i = 0
   for j in range(4):  # Goes through loop 2, i = 0 and j = 0
     print(i, j)       # Prints (i, j) = 0 0, this repeats until the first loop is finished
 
+# WHILE LOOP
+
+#Example 01
+i = 0
+while i <= 10:
+  print("hello")
+  i += 1
+
+# Example 02
+while True:
+  print("Yes")
+
+# Example 03
+count = 0
+while True:
+  if count < 10:
+    continue
+  print(count)
